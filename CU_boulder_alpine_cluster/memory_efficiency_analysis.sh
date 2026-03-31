5e010299f08d502abc44575811df6fcd14d04ade
Andrew John Buggee <anbu8374@colorado.edu>
2026-03-31 11:13:20 -0600

    updates to run shell script

--- CU_boulder_alpine_cluster/memory_efficiency_analysis.sh
+++ CU_boulder_alpine_cluster/memory_efficiency_analysis.sh
@@ -2,6 +2,23 @@
 # Script to collect memory efficiency from Alpine seff command
 # Extracts memory efficiency % from all jobs in the array 24757758_101 to 24757758_173
 
+# %A: Job ID
+# %a: Array Task ID
+# ----------------------------------------------------------
+#SBATCH --account=ucb762_asc1                   # Ascent Allocation on Alpine
+#SBATCH --nodes=1
+#SBATCH --time=01:00:00   # Request 1 hour for data collection
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
+#SBATCH --mem=10G        # Should be closer to 80% efficiency based on previous runs, but giving some buffer for variability
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name=mem_collection
+#SBATCH --output=mem_collection.out
+#SBATCH --error=mem_collection.err
+#SBATCH --mail-user=anbu8374@colorado.edu
+#SBATCH --mail-type=ALL
+
 output_file="memory_efficiency_data.txt"
 > "$output_file"  # Clear file
 
